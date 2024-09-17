8ed1a80fce51871530e0f184980963d8b10cbe0c
Barnabas Kendall <barnabas.kendall@gmail.com>
2024-09-17 13:45:16 -0400

    expr listener walker

--- lexers/expr/Driver.py
+++ lexers/expr/Driver.py
@@ -2,7 +2,16 @@ import sys
 from antlr4 import *
 from ExprLexer import ExprLexer
 from ExprParser import ExprParser
-#from VisitorInterp import VisitorInterp
+from ExprListener import ExprListener
+
+class ExprPrintListener(ExprListener):
+    # def enterProg(self, ctx):
+    #     print(ctx.expr())
+
+    def enterExpr(self, ctx):
+        token = ctx.INT()
+        if token is not None:
+            print(ctx.INT())
 
 def main(argv):
     input_stream = FileStream(argv[1])
@@ -10,7 +19,9 @@ def main(argv):
     stream = CommonTokenStream(lexer)
     parser = ExprParser(stream)
     tree = parser.prog()
-    print(tree)
+    printer = ExprPrintListener()
+    walker = ParseTreeWalker()
+    walker.walk(printer, tree)
 
 if __name__ == '__main__':
     main(sys.argv)
