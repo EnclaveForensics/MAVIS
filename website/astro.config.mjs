40feb6d6fb393735dcf89d97622f12a5ac1337f8
Barnabas Kendall <barnabas.kendall@gmail.com>
2024-11-10 10:52:35 -0500

    website init with starlight

---
+++ website/astro.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "MAVIS",
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
+      social: {
+        github: "https://github.com/EnclaveForensics/MAVIS",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
